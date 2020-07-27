6905a2a620efce34d9ab3f0886005c5a80eb0597
jollyr0ger <kav993@gmail.com>
2020-07-27 17:19:07 +0300

    change gameGrid from bollean to binary style

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,16 +5,35 @@ import Board from '../Board/Board';
 function Game(props) {
   // array of array with columns * rows dimension
   let [gameGrid, setGameGrid] = useState(Array.from(
-    {length: props.rows}, () => new Array(props.columns).fill(false))
+    {length: props.rows}, () => new Array(props.columns).fill(0))
   );
+  let [intervalID, setIntervalID] = useState();
+
+  function handleStart(){
+    if(intervalID){
+      clearInterval(intervalID)
+      setIntervalID(undefined)
+    } else{
+      const interval = setInterval(() => console.log('start'), 500);
+      setIntervalID(interval)
+    }
+  }
 
   function handleClick(row, column) {
     let grid = gameGrid.slice();
-    grid[row][column] = !grid[row][column]
+    grid[row][column] = grid[row][column] ? 0 : 1
     setGameGrid(grid)
   }
 
-  return (<Board gameGrid={gameGrid} handleClick={handleClick} />)
+  return (
+    <div>
+      <button onClick={handleStart}>Start</button>
+      <Board 
+        gameGrid={gameGrid} 
+        handleClick={handleClick} 
+      />
+    </div>
+  )
 }
 
 export default Game;
